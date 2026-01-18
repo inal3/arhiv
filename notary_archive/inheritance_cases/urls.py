23476edeb6a8ac9982f8cad4219e8ad22015ebab
Marat Dzodziev <minamota80@gmail.com>
2026-01-18 20:45:31 +0300

    Добавлена карточка наследственного дела

--- notary_archive/inheritance_cases/urls.py
+++ notary_archive/inheritance_cases/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index_ic, name='index_ic'),
+    path('1/', views.detail_ic, name='detail_ic')
 
 ]
--- notary_archive/inheritance_cases/views.py
+++ notary_archive/inheritance_cases/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index_ic(request):
     return render(request, 'inheritance_cases/index_ic.html')
+
+def detail_ic(request):
+    return render(request,'inheritance_cases/inheritance_card.html')
